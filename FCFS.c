36af909b4aec692e69d925a6f2a7335cb46d92b0
Mehedi Hasan <mehedihasansabbirmi@gmail.com>
2018-07-11 20:36:35 +0600

    Create FCFS Program of OS 1

---
+++ FCFS.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+struct Process
+{
+    int burstTime;
+};
+
+float countAvgWaitingTime(float avg, int count){
+    return avg / count;
+}
+
+int main(){
+    int processCount = 0, i;
+    float avgWaititngTime = 0.0, waitingTime;
+
+    struct Process processes[100];
+    
+    printf("Enter process count:");
+    scanf("%d", &processCount);
+    printf("Enter burst time: \n\n");
+    for(i = 0; i < processCount; i++){
+        printf("Enter process %d burst time: ", i+1);
+        scanf("%d", &processes[i].burstTime);
+    }
+    printf("\nWaiting time:\n");
+    for(i = 0; i < processCount; i++){
+        if(i == 0){
+            printf("Process 1 waiting time: 0\n");
+            continue;
+        }
+        waitingTime = (processes[i - 1].burstTime + avgWaititngTime);
+        printf("Process %d waiting time: %f\n", (i + 1), waitingTime);
+    
+        avgWaititngTime += waitingTime;
+    }
+    printf("\n");
+    printf("\nAverage waiting time: %f\n", countAvgWaitingTime(avgWaititngTime, processCount));
+    return 0;
+}
